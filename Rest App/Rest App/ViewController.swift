23b11dd0abb2456cda9858778098e52ca81d8677
Ilya Pugachev <ilyapugachv@gmail.com>
2023-10-11 13:45:09 +0300

    Project created

---
+++ Rest App/Rest App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rest App
+//
+//  Created by Илья Пугачёв on 11.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
